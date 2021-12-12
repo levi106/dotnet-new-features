a5c160979f310b05f07f85ce5aefcbc7f1a23b95
Hiroyuki Hasebe <hiroyha@microsoft.com>
2021-12-12 21:43:36 +0900

    add some perf test

---
+++ sealed-class-net6-perf/Program.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, DefaultConfig.Instance);
+
+[DisassemblyDiagnoser(maxDepth: 1)]
+[MemoryDiagnoser(displayGenColumns: false)]
+public partial class Program
+{
+    private SealedType _sealed = new();
+    private NonSealedType _nonSealed = new();
+
+    [Benchmark(Baseline = true)]
+    public int NonSealed() => _nonSealed.M() + 42;
+
+    [Benchmark]
+    public int Sealed() => _sealed.M() + 42;
+}
+
+public class BaseType
+{
+    public virtual int M() => 1;
+}
+
+public class NonSealedType : BaseType
+{
+    public override int M() => 2;
+}
+
+public sealed class SealedType : BaseType
+{
+    public override int M() => 2;
+}
---
+++ sealed-type-check-net6-perf/Program.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, DefaultConfig.Instance);
+
+[DisassemblyDiagnoser(maxDepth: 1)]
+[MemoryDiagnoser(displayGenColumns: false)]
+public partial class Program
+{
+    private object _o = "hello";
+
+    [Benchmark(Baseline = true)]
+    public bool NonSealed() => _o is NonSealedType;
+
+    [Benchmark]
+    public bool Sealed() => _o is SealedType;
+}
+
+public class NonSealedType { }
+public sealed class SealedType { }
