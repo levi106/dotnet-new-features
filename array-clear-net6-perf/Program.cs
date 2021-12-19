820be640551e33f926d906d19af88fe6cfd4d290
Hiroyuki Hasebe <hiroyha@microsoft.com>
2021-12-19 20:39:15 +0900

    add more perfomance tests

---
+++ array-clear-net6-perf/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, DefaultConfig.Instance);
+
+[DisassemblyDiagnoser(maxDepth: 1)]
+[MemoryDiagnoser(displayGenColumns: false)]
+public partial class Program
+{
+    private int[] _array = new int[10];
+
+    [Benchmark(Baseline = true)]
+    public void Old() => Array.Clear(_array, 0, _array.Length);
+
+    [Benchmark]
+    public void New() => Array.Clear(_array);
+}
---
+++ process-path-net6-perf/Program.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Diagnostics;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, DefaultConfig.Instance);
+
+[DisassemblyDiagnoser(maxDepth: 1)]
+[MemoryDiagnoser(displayGenColumns: false)]
+public partial class Program
+{
+    [Benchmark(Baseline = true)]
+    public string GetPathViaProcess() =>
+        Process.GetCurrentProcess().MainModule.FileName;
+
+    [Benchmark]
+    public string GetPathViaEnvironment() =>
+        Environment.ProcessPath;
+}
+
+var data = await File.ReadAllBytesAsync(@"c:large_file.json");
+
+// Safe but slow.
+var copied = ByteString.CopyFrom(data);
+
+// Unsafe but fast. Useful if you know data won't change.
+var wrapped = UnsafeByteOperations.UnsafeWrap(data);
